8ceba562b25b214a64d45446161f491d598d532d
Jay Zhou <jzilla808@gmail.com>
2023-05-15 23:33:43 -0700

    Improved loading animation

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ function Loading() {
   return (
     <div className="bg-gradient-to-br from-[#394F68] to-[#183B7E] min-h-screen flex flex-col items-center justify-center text-slate-500">
       <SunIcon
-        className="h-24 w-24 animated-bounce text-yellow-500"
+        className="h-24 w-24 animate-bounce text-yellow-500"
         color="yellow"
       />
       <h1 className="text-4xl sm:text-6xl font-bold text-center mb-10 animate-pulse">
